934a618c4c29e30a158a0a0e4315792dc8fa785a
Masashi <my.caveman0630@gmail.com>
2024-06-09 10:06:54 +0900

    cart implemented

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,5 @@
-import { View } from 'react-native';
+import { Redirect } from "expo-router";
 
-import products from '@/assets/data/products';
-import ProductListItem from '@/components/ProductListItem';
-
-export default function TabOneScreen() {
-  return (
-    <View>
-      <ProductListItem product={products[5]} />
-      <ProductListItem product={products[1]} />
-    </View>
-  );
+export default function TabIndex() {
+    return <Redirect href={'/menu'} />
 }
--- components/ProductListItem.tsx
+++ components/ProductListItem.tsx
@@ -1,18 +1,27 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import { Link } from 'expo-router';
 import Colors from '@/constants/Colors';
+import { Product } from '@/types';
 
-const ProductListItem = ({ product }) => {
+export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png';
+
+type ProductListItemProps = {
+    product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
     return (
-        <View style={styles.container}>
-            <Image
-                source={{ uri: product.image }}
-                style={styles.image}
-                resizeMode="contain"
-            />
-            <Text style={styles.title}>{product.name}</Text>
-            <Text style={styles.price}>${product.price.toFixed(2)}</Text>
-        </View>
+        <Link href={`/menu/${product.id}`} asChild>
+            <Pressable style={styles.container}>
+                <Image
+                    source={{ uri: product.image || defaultPizzaImage }}
+                    style={styles.image}
+                    resizeMode="contain"
+                />
+                <Text style={styles.title}>{product.name}</Text>
+                <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+            </Pressable>
+        </Link>
     )
 }
 
@@ -24,6 +33,8 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         padding: 10,
         overflow: 'hidden',
+        flex: 1,
+        maxWidth: '50%',
     },
     image: {
         width: '100%',
